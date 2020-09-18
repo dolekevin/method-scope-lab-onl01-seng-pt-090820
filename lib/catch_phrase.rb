46e59102f43d1529cbb6357b69814729c075e802
Kevin Dole <dole.kevin@gmail.com>
2020-09-18 11:21:55 +0000

    Automatically backed up by Learn

--- lib/catch_phrase.rb
+++ lib/catch_phrase.rb
@@ -5,5 +5,4 @@ describe "#catch_phrase" do
   it "puts out a catch phrase" do
     phrase = "It's-a me, Mario!"
     expect{catch_phrase(phrase)}.to output("It's-a me, Mario!\n").to_stdout
-  end
 end
